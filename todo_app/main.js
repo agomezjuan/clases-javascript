ae9f2e3020eb69341e2b322a0420627e32c5a1be
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-15 12:06:54 +0100

    avances 15-02-2023

--- todo_app/main.js
+++ todo_app/main.js
@@ -5,6 +5,22 @@ const ul = document.createElement('ul');
 
 h1.textContent = 'To Do App';
 
+// javascript object notation : json
+const imagenes = [
+    {
+        imagen: './img/1.jpeg',
+        alt: 'Imagen 1'
+    },
+    {
+        imagen: 'https://picsum.photos/200/300',
+        alt: 'Imagen 2'
+    },
+    {
+        imagen: 'https://picsum.photos/200/300',
+        alt: 'Imagen 3'
+    },
+]
+
 app.appendChild(h1);
 
 
@@ -16,7 +32,9 @@ const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 for (let i = 0; i < tasks.length; i++) {
     const element = tasks[i];
     const li = document.createElement('li');
-    li.textContent = element;
+    //li.textContent = element + '<i class="fa-solid fa-trash"></i>';
+    li.innerHTML = element + '<i class="fa-solid fa-trash"></i>';
+    //li.innerText = element + '<i class="fa-solid fa-trash"></i>';
     ul.appendChild(li);
 }
 
@@ -37,7 +55,7 @@ app.appendChild(form);
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     const li = document.createElement('li');
-    li.textContent = input.value;
+    li.innerHTML = input.value + '<i class="fa-solid fa-trash"></i>';
     ul.appendChild(li);
     tasks.push(input.value);
     console.log(tasks);
@@ -48,9 +66,53 @@ form.addEventListener('submit', function (e) {
 app.appendChild(ul);
 
 // Evento click
-ul.addEventListener('click', function (e) {
-    const element = e.target;
-    if (element.tagName === 'LI') {
-        element.remove();
-    }
-});
+const icons = document.querySelectorAll('i');
+
+icons.forEach(function (icon) {
+    icon.addEventListener('click', function (e) {
+        const tarea = e.target;
+        let confimar = confirm('¿Estás seguro de eliminar la tarea?');
+        if (confimar) {
+            tarea.parentElement.remove();
+        }
+    })
+})
+
+const titleImagenes = document.createElement('h2');
+
+titleImagenes.textContent = 'Imagenes';
+
+app.appendChild(titleImagenes);
+
+// for (let i = 0; i < imagenes.length; i++) {
+//     const imagen = imagenes[i];
+//     const img = document.createElement('img');
+//     img.setAttribute('src', imagen.imagen);
+//     img.setAttribute('alt', imagen.alt);
+//     app.appendChild(img);
+
+// }
+const imgContainer = document.createElement('section');
+
+imagenes.forEach(function (imagen) {
+    const picture = document.createElement('picture');
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = imagen.alt;
+
+    const img = document.createElement('img');
+    img.setAttribute('src', imagen.imagen);
+    img.setAttribute('alt', imagen['alt']);
+    img.setAttribute('class', 'imagen')
+
+    picture.appendChild(img);
+    picture.appendChild(figcaption);
+
+    imgContainer.appendChild(picture);
+
+
+})
+
+app.appendChild(imgContainer);
+
+console.log(ul.children);
+console.log(imgContainer.firstChild);

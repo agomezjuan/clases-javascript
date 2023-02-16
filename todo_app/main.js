5c63a8e7302284c56ff56b5ecb20f2c8d52fe2f3
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-02-16 12:04:57 +0100

    avances 16-02-2023

--- todo_app/main.js
+++ todo_app/main.js
@@ -6,20 +6,7 @@ const ul = document.createElement('ul');
 h1.textContent = 'To Do App';
 
 // javascript object notation : json
-const imagenes = [
-    {
-        imagen: './img/1.jpeg',
-        alt: 'Imagen 1'
-    },
-    {
-        imagen: 'https://picsum.photos/200/300',
-        alt: 'Imagen 2'
-    },
-    {
-        imagen: 'https://picsum.photos/200/300',
-        alt: 'Imagen 3'
-    },
-]
+const imagenes = JSON.parse(localStorage.getItem('imagenes')) || []
 
 app.appendChild(h1);
 
@@ -32,36 +19,30 @@ const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 for (let i = 0; i < tasks.length; i++) {
     const element = tasks[i];
     const li = document.createElement('li');
-    //li.textContent = element + '<i class="fa-solid fa-trash"></i>';
     li.innerHTML = element + '<i class="fa-solid fa-trash"></i>';
-    //li.innerText = element + '<i class="fa-solid fa-trash"></i>';
     ul.appendChild(li);
 }
 
 // Formulario
 const form = document.createElement('form');
-const input = document.createElement('input');
-input.setAttribute('type', 'text');
+const inputTitle = document.createElement('input');
+const inputImageUrl = document.createElement('input');
+inputTitle.setAttribute('type', 'text');
+inputImageUrl.setAttribute('type', 'text');
+inputTitle.setAttribute('placeholder', 'Titulo');
+inputImageUrl.setAttribute('placeholder', 'Url de imagen');
 
 const button = document.createElement('button');
 button.setAttribute('type', 'submit');
 button.textContent = 'Crear';
 
-form.appendChild(input);
+form.appendChild(inputTitle);
+form.appendChild(inputImageUrl);
 form.appendChild(button);
 
 app.appendChild(form);
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const li = document.createElement('li');
-    li.innerHTML = input.value + '<i class="fa-solid fa-trash"></i>';
-    ul.appendChild(li);
-    tasks.push(input.value);
-    console.log(tasks);
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-    input.value = '';
-})
+
 
 app.appendChild(ul);
 
@@ -74,6 +55,9 @@ icons.forEach(function (icon) {
         let confimar = confirm('¿Estás seguro de eliminar la tarea?');
         if (confimar) {
             tarea.parentElement.remove();
+            const indexToDelete = tasks.indexOf(tarea.parentElement.textContent);
+            tasks.splice(indexToDelete, 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks))
         }
     })
 })
@@ -110,6 +94,37 @@ imagenes.forEach(function (imagen) {
     imgContainer.appendChild(picture);
 
 
+})
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // const li = document.createElement('li');
+    // li.innerHTML = input.value + '<i class="fa-solid fa-trash"></i>';
+    // ul.appendChild(li);
+    // tasks.push(input.value);
+    // console.log(tasks);
+    const imagen = {
+        imagen: inputImageUrl.value,
+        alt: inputTitle.value
+    }
+    imagenes.push(imagen);
+    const picture = document.createElement('picture');
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = imagen.alt;
+
+    const img = document.createElement('img');
+    img.setAttribute('src', imagen.imagen);
+
+    img.setAttribute('class', 'imagen')
+
+    picture.appendChild(img);
+    picture.appendChild(figcaption);
+
+    imgContainer.appendChild(picture);
+
+
+    localStorage.setItem("imagenes", JSON.stringify(imagenes))
+    inputTitle.value = '';
+    inputImageUrl.value = '';
 })
 
 app.appendChild(imgContainer);
